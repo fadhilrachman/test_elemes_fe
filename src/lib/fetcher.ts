e8c635bb6051f3e7f564271f4b4e01410d29ac98
fadhilrachman <muhfadhilrachman@gmail.com>
2025-08-28 10:20:30 +0700

    fix: fixed env variable

--- src/lib/fetcher.ts
+++ src/lib/fetcher.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 const fetcher = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
   headers: {
-    Authorization: `Bearer ${import.meta.env.VITE_API_KEY}`,
+    Authorization: `Bearer ${import.meta.env.VITE_API_KEY_TMDB}`,
   },
 });
 
